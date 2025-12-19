3e058270aff0b2f5df38e143f5a8ae9eadede134
wbwbwywyb-ship-it <wbwbwywyb@icloud.com>
2025-12-19 18:07:19 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // 必须与你的 GitHub 仓库名完全一致
+  // 必须与你的 GitHub 仓库名称一致
   base: '/birthday-webwyl24/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    cssCodeSplit: false
+    emptyOutDir: true,
+    target: 'esnext'
   }
 });
