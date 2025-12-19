a1397782ef9da8cd8241816870d61c56bc87003e
wbwbwywyb-ship-it <wbwbwywyb@icloud.com>
2025-12-19 17:58:55 +0800

    Update store.ts

--- store.ts
+++ store.ts
@@ -2,19 +2,20 @@
 import { create } from 'zustand';
 import { AppState, Phase, Gesture, PhotoData } from './types';
 
-// Default placeholder photos
 const INITIAL_PHOTOS: PhotoData[] = Array.from({ length: 24 }).map((_, i) => ({
   id: `default-${i}`,
   url: `https://picsum.photos/seed/${i + 100}/800/1000`,
   isLandscape: false,
 }));
 
-export const useStore = create<AppState>((set) => ({
+export const useStore = create<AppState & { isLoading: boolean; setIsLoading: (val: boolean) => void }>((set) => ({
   phase: Phase.CAKE,
   gesture: Gesture.NONE,
   photos: INITIAL_PHOTOS,
   cameraEnabled: false,
   selectedPhotoIndex: null,
+  isLoading: true,
+  setIsLoading: (isLoading) => set({ isLoading }),
   setPhase: (phase) => set({ phase }),
   setGesture: (gesture) => set({ gesture }),
   toggleCamera: () => set((state) => ({ cameraEnabled: !state.cameraEnabled })),
